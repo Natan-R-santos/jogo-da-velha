fcc342a0d5038de0f199e61d5d22cae007418955
Dev Natan Ramos <nathanrcs2022@gmail.com>
2025-12-08 17:59:34 -0300

    alteraçao do projeto e finalizar por completo

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
-alert ("Feito por Natan Rcs");
-// pega todas as divs com a classe Div1
-const Divs = document.querySelectorAll("divs1");
-// variavel que permite controlar de quem é a vez
-let vez = "X";
+function quemfez(){
+    alert("feito por natan");
+}
+quemfez();
+
+// pega os elementos com query selector todas as classes com o nome divs1
+const divs = document.querySelectorAll(".divs1");
+// variavel que contem o jogador da vez 
+let vezdojogador = "X";
+// usamos o foreach para percorrer as 9 divs e colocamos o evento de click,caso esteja vazio coloca qual é o jogador pela variavel
+// ai caso for a vez do X alteramos a vez do jogador vezdojogador = (vezdojogador === "X") ? "O" : "X"; caso for o bolinha troca para o X e vice versa
+divs.forEach(divs => {
+    divs.addEventListener("click", () => {
+        if(divs.textContent ===""){
+            divs.textContent = vezdojogador;
+            vezdojogador = (vezdojogador === "X") ? "O" : "X";
+        }
+    });
+});
+
+
