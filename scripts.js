f2593bdf33cab87b1fc12f8f9eae3ce6857dc4e8
Dev Natan Ramos <nathanrcs2022@gmail.com>
2025-12-08 21:21:38 -0300

    Erro corrigido com o uso de .trim() que tira os espaçamentos e quebras de linhas invisivel,e conseguimos mostrar na tela os elementos X e O no jogo da velha.lembrando que foi a base dos estudos aplicado na pratica e deu resultado bom,mais pra frente serao acrescentados mais funcionalidades ao jogo!

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ function quemfez() {
 }
 quemfez();
 
-// pega as 9 posições do tabuleiro 
-const divs = document.querySelectorAll(".div2");
+// pega as 9 posições do tabuleiro do jogo 
+const divs = document.querySelectorAll(".div2"); // <<< CORRIGIDO
 
-// jogador inicial
+// jogador inicial que começa 
 let vezdojogador = "X";
 
-// combinações de vitorias vencedoras
+// combinações de vitórias
 const listaVitorias = [
     [0, 1, 2],
     [3, 4, 5],
@@ -21,31 +21,31 @@ const listaVitorias = [
     [2, 4, 6]
 ];
 
-// adiciona evento de clique para cada posição/div
-divs.forEach((div, index) => {
+// adiciona evento de clique para cada div colocando o elemento da vez
+divs.forEach((div, index) => {   // <<< Tirado a vírgula extra
     div.addEventListener("click", () => {
-        console.log("teste teste")
+// o trim retira os espaços em brancos e quebras de linhas se tiver ele limpa e coloca o x ou o dentro da div
+        if (div.textContent.trim() === "") {
 
-        if (div.textContent === "") {
+            div.textContent = vezdojogador;
+            div.style.color = "purple";
 
-            divs.textContent = vezdojogador;
-            divs.style.color= "purple";
-
-            // verifica se ganhou ou nao
+            // verifica se ganhou ou não é uma condiçao
             if (veriqueVitoria(vezdojogador)) {
                 alert(`Jogador ${vezdojogador} venceu!`);
                 return;
             }
 
-            // troca jogador
+            // troca jogador qnd a div estiver ocupada 
             vezdojogador = (vezdojogador === "X") ? "O" : "X";
         }
     });
 });
 
-// função para verificar vitórias 
+// função que verifica vitórias de quem conseguir bater a combinaçao de acertos.
 function veriqueVitoria(jogador) {
     return listaVitorias.some(comb => {
         return comb.every(pos => divs[pos].textContent === jogador);
     });
 };
+
